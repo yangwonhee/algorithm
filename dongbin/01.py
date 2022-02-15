c83e8b54fd9a1f784067268f7f96953867186fa3
yangwonhee <yangwonhee@kakao.com>
2022-02-15 16:45:08 +0900

    이코테1

---
+++ dongbin/01.py
@@ -0,0 +1,13 @@
+data = dict()
+data['tkrhk'] = 'apple'
+data['qksksk'] = 'banana'
+data['zhzhsjt'] = 'coconut'
+
+key_list = data.keys()
+value_list = data.values()
+
+print(key_list)
+print(value_list)
+
+for key in key_list:
+    print(data[key])
