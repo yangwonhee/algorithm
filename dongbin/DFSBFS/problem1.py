5a61f8ca39f498d4ef28d95e8872f862d66c461a
yangwonhee <yangwonhee@kakao.com>
2022-02-26 16:57:03 +0900

    dfs bfs 1++

--- dongbin/DFSBFS/problem1.py
+++ dongbin/DFSBFS/problem1.py
@@ -1,12 +1,11 @@
-import sys
+n, m = map(int, input().split())
 
-n, m = map(int, sys.stdin.readline().split())
 graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
 
 def dfs(x, y):
-    if x <= -1 or x >= n or y <= -1 or y >= m:
+    if x <= -1 or x >= n or y <= 1 or y >= n:
         return False
     if graph[x][y] == 0:
         graph[x][y] = 1
@@ -22,4 +21,5 @@ for i in range(n):
     for j in range(m):
         if dfs(i, j) == True:
             result += 1
+
 print(result)
--- dongbin/DFSBFS/problem2.py
+++ dongbin/DFSBFS/problem2.py
@@ -1,9 +1,7 @@
-import sys
 from collections import deque
-n, m = map(int, sys.stdin.readline().split())
+
+n, m = map(int, input().split())
 graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
 
 def bfs(x, y):
     queue = deque()
@@ -12,16 +10,18 @@ def bfs(x, y):
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
-            ny = x + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >=n:
                 continue
             if graph[nx][ny] == 0:
                 continue
             if graph[nx][ny] == 1:
                 graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
+                queue.append(nx, ny)
     return graph[n-1][m-1]
 
+for _ in range(n):
+    graph.append(list(map(int, input())))
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
